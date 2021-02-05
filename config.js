eac4a9f08248091ce63d63b34ec8d0c35e960330
charliepoker <iheanachocharlie@gmail.com>
2021-02-05 15:59:02 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+const URL = {
+  API_URL:
+    ' https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=34a481b01a2b92d94d14f8ef49db1be7&page=1',
+  IMG_PATH: 'https://image.tmdb.org/t/p/w1280',
+  SEARCH_API:
+    'https://api.themoviedb.org/3/search/movie?&api_key=34a481b01a2b92d94d14f8ef49db1be7&query="',
+};
